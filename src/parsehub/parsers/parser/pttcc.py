56f4f9d09fe8ad5dc85826bf8bea4a37bceb444f
tdc <tdc@sudo.host>
2025-11-11 16:21:50 +0800

    ✨ feat(parser/pttcc): 修正 imgur 下載時需要 ua 的問題

--- src/parsehub/parsers/parser/pttcc.py
+++ src/parsehub/parsers/parser/pttcc.py
@@ -1,6 +1,10 @@
 from ..base.base import BaseParser
 from ...types import Image, ImageParseResult
 from ...provider_api.pttcc import PTTCC
+from ...config import DownloadConfig, GlobalConfig
+from collections.abc import Awaitable, Callable
+from pathlib import Path
+from ...types import DownloadResult, ImageParseResult
 
 class PTTParser(BaseParser):
     __platform_id__ = "ptt"
@@ -25,5 +29,19 @@ class PTTImageParseResult(ImageParseResult):
     def __init__(self, title: str, photo: list[str], desc: str, raw_url: str, pttcc: "PTTCC"):
         super().__init__(title, photo, desc, raw_url)
         self.pttcc = pttcc
+    
+    async def download(
+        self,
+        path: str | Path = None,
+        callback: Callable[[int, int, str | None, tuple], Awaitable[None]] = None,
+        callback_args: tuple = (),
+        config: DownloadConfig = DownloadConfig(),
+    ) -> DownloadResult:
+        headers = config.headers or {"User-Agent": GlobalConfig.ua}
+        headers["Accept"] = (
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+        )
+        config.headers = headers
+        return await super().download(path, callback, callback_args, config)
 
 __all__ = ["PTTParser", "PTTImageParseResult"]
